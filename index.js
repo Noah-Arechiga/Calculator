6895a5786d7c2fd1c870a6aefb6bf04583bd6262
Noah-Arechiga <narechiga@ucsd.edu>
2025-08-20 22:18:19 -0700

    Added functions for new operations

--- index.js
+++ index.js
@@ -11,10 +11,34 @@ function clearDisplay() {
     display.value = "";
 }
 
+// When the user presses "DEL", it will delete the last character entered.
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+// When the user presses "+/-", it will change the sign of the last number entered.
+function changeSign() {
+    // If the display is empty, just return
+    if (display.value === "") return;
+
+    // Try to parse the last number entered
+    let match = display.value.match(/(-?\d+\.?\d*)$/);
+    if (match) {
+        let number = parseFloat(match[0]);
+        let toggled = -number;
+        display.value = display.value.slice(0, match.index) + toggled;
+    }
+}
+
 // This function calculates the possible operations.
 function calculate() {
     try {
-        display.value = eval(display.value);
+        let expression = display.value;
+
+        // Replace ^ with ** for exponentiation
+        expression = expression.replace(/\^/g, "**");
+
+        display.value = eval(expression);
     }
     catch(error) {
         display.value = "Error";
